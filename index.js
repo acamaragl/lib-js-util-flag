84e12a6fc127aebbbdb481141015d37b2b09a95c
Alberto C <alberto.camara@bitfinex.com>
2025-08-28 12:05:29 +0200

    feat: remove flag bfx-pay. Final decommission

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ const FLAGS = {
     DEBUG: 2 ** 19,
     C_DEBUG: 2 ** 20,
     DP_TRAVEL_RULE: 2 ** 21,
-    DP_BFXPAY: 2 ** 22,
     APP_OFFICIAL: 2 ** 25,
     APP_MOBILE_OFFICIAL: 2 ** 26,
     VNEXT_0: 2 ** 29
